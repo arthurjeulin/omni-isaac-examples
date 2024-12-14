8dd963af3f49b52018b46ab5bc55b5a20d044330
ajeulin <arthur.jeulin@coastautonomous.com>
2024-12-14 18:23:56 -0500

    first commit

---
+++ omni/isaac/examples/hello_world/hello_world.py
@@ -0,0 +1,22 @@
+from omni.isaac.examples.base_sample import BaseSample
+import numpy as np
+# Can be used to create a new cube or to point to an already existing cube in stage.
+from omni.isaac.core.objects import DynamicCuboid
+
+class HelloWorld(BaseSample):
+    def __init__(self) -> None:
+        super().__init__()
+        return
+
+    def setup_scene(self):
+        world = self.get_world()
+        world.scene.add_default_ground_plane()
+        fancy_cube = world.scene.add(
+            DynamicCuboid(
+                prim_path="/World/random_cube", # The prim path of the cube in the USD stage
+                name="fancy_cube", # The unique name used to retrieve the object from the scene later on
+                position=np.array([0, 0, 1.0]), # Using the current stage units which is in meters by default.
+                scale=np.array([0.5015, 0.5015, 0.5015]), # most arguments accept mainly numpy arrays.
+                color=np.array([0, 0, 1.0]), # RGB channels, going from 0-1
+            ))
+        return
